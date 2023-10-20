2d965c2558ab1874da2eb38685d64b9cd7857a4e
Nghi <nghinvps23655@fpt.edu.vn>
2023-10-20 17:53:33 +0700

    update

--- app/Livewire/Clients/Education/Create.php
+++ app/Livewire/Clients/Education/Create.php
@@ -2,25 +2,43 @@
 
 namespace App\Livewire\Clients\Education;
 
+use App\Models\CoursesModel;
 use Livewire\Component;
 
 class Create extends Component
 {
     public $step = 1;
     public $course_name;
+    public $category;
+    public $payment;
+    public $description;
 
     public function next()
     {
         if ($this->step < 3) {
-            $this->step++;
+            $this->step=$this->step+1;
         }
     }
 
     public function back()
     {
         if ($this->step > 1) {
-            $this->step--;
+            $this->step=$this->step-1;
         }
+    }
+    public function create(){
+        $education = CoursesModel::create([
+            'course_name'=>$this->course_name,
+            'category'=>$this->category,
+            'payment'=>$this->payment,
+            'description'=>$this->description,
+        ]);
+        $this->step=1;
+        $this->course_name='';
+        $this->category='';
+        $this->payment='';
+        $this->description='';
+        session()->flash('message', 'Education Created Successfully.');
     }
     public function render()
     {
--- resources/views/livewire/clients/education/create.blade.php
+++ resources/views/livewire/clients/education/create.blade.php
@@ -29,12 +29,15 @@
                         <div class="line">
                             <input wire:model="course_name" class="line__input" id="course_name" name="course_name"
                                 type="text" value="" autocomplete="off">
-                            <span for="username" class="line__placeholder"> Tên khóa học </span>
+                            @if (is_null($course_name))
+                                <span for="username" class="line__placeholder"> Tên khóa học </span>
+                            @endif
                         </div>
 
                         <div>
                             <label for="" class="font-semibold text-base">Danh mục</label>
-                            <select id="" name="" class="shadow-none selectpicker with-border">
+                            <select id="" name="category" class="shadow-none selectpicker with-border"
+                                wire:model="category">
                                 <option value="1">HTML</option>
                                 <option value="2">CSS</option>
                                 <option value="3">JavaScript</option>
@@ -42,7 +45,7 @@
                         </div>
 
                         <div class="line h-32">
-                            <textarea class="line__input h-32" id="" name="" type="text"
+                            <textarea class="line__input h-32" id="" name="description" wire:model="description" type="text"
                                 onkeyup="this.setAttribute('value', this.value);" value="" autocomplete="off"></textarea>
                             <span for="username" class="line__placeholder"> Mô tả </span>
                         </div>
@@ -51,9 +54,10 @@
                             <label for="" class="font-semibold text-base">Tinh phí</label>
                             <div> Nếu bật tính phí người dùng phải trả tiền mới có thể tham gia khóa học
                                 của bạn</div>
-                            <select id="" name="" class="shadow-none selectpicker with-border">
-                                <option data-icon="uil-bullseye">Có</option>
-                                <option data-icon="uil-chat-bubble-user">Không</option>
+                            <select id="" name="payment" class="shadow-none selectpicker with-border"
+                                wire:model='payment'>
+                                <option data-icon="uil-bullseye" value="0">Có</option>
+                                <option data-icon="uil-chat-bubble-user" value="1">Không</option>
                             </select>
                         </div>
 
@@ -72,19 +76,18 @@
 
                     <!-- form header -->
                     <div class="text-center border-b border-gray-100 py-6">
-                        <h3 class="font-bold text-xl"> Tạo khóa học </h3>
+                        <h3 class="font-bold text-xl">Banner</h3>
                     </div>
 
                     <!-- form body -->
                     <div class="p-10 space-y-7">
-
-
+                        <input wire:model="banner" class="form-control" name="banner" type="file">
 
                     </div>
 
                     <!-- form footer -->
-                    <div class="flex justify-center border-gray-100 py-8">
-                        <button wire:click="next" type="button" class="button lg:w-1/3">
+                    <div class="flex justify-content-between border-gray-100 p-8">
+                        <button wire:click="back" type="button" class="button lg:w-1/3">
                             Quay lại
                         </button>
                         <button wire:click="next" type="button" class="button lg:w-1/3">
@@ -103,24 +106,53 @@
 
                     <!-- form body -->
                     <div class="p-10 space-y-7">
-
-
-
+                        <div class="mb-3 container-input">
+                            <label for="exampleFormControlInput1" class="form-label">Yêu cầu:</label>
+                            <input type="text" class="form-control"
+                                placeholder="Kiến thức cần có để tham gia khóa học">
+                            <button class="btn mx-2" id="addInput1">+</button>
+                        </div>
+                        <div class="mb-3 container-input">
+                            <label for="exampleFormControlInput1" class="form-label">Nội dung khóa học:</label>
+                            <input type="text" class="form-control" placeholder="Kiến thức học được">
+                            <button class="btn mx-2" id="addInput2">+</button>
+                        </div>
                     </div>
-
-                    <!-- form footer -->
+                      <!-- form footer -->
                     <div class="flex justify-content-between border-gray-100 p-8">
                         <button wire:click="back" type="button" class="button lg:w-1/3">
                             Quay lại
                         </button>
-                        <button wire:click="next" type="button" class="button lg:w-1/3">
+                        <button wire:click="Create" type="button" class="button lg:w-1/3">
                             Đăng kí
                         </button>
                     </div>
-
                 </div>
-            @endif
+
 
         </div>
+        @endif
+
     </div>
 </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.7.1.js" crossorigin="anonymous"></script>
+<script>
+    // Sử dụng jQuery để thêm trường nhập liệu khi bấm nút "+"
+    $(document).ready(function() {
+        $("#addInput1").click(function() {
+            const newInput = $("<input/>", {
+                type: "text",
+                class: "form-control",
+            });
+            newInput.insertBefore(this);
+        });
+        $("#addInput2").click(function() {
+            const newInput = $("<input/>", {
+                type: "text",
+                class: "form-control",
+            });
+            newInput.insertBefore(this);
+        });
+    });
+</script>
