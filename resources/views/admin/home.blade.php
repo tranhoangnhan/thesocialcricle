76497e0e9f47f528a45a7178e1504eec74c6c401
Nghi <nghinvps23655@fpt.edu.vn>
2023-12-12 10:25:14 +0700

    update admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,150 @@
+@extends('layouts.admin')
+    @section('content')
+        <div class="row">
+            <div class="col">
+
+                <div class="h-100">
+                    <div class="row mb-3 pb-1">
+                        <div class="col-12">
+                            <div class="d-flex align-items-lg-center flex-lg-row flex-column">
+                                <div class="flex-grow-1">
+                                    <h4 class="fs-16 mb-1">Xin chào, {{ auth()->user()->user_fullname }}!</h4>
+                                    <p class="text-muted mb-0">Dữ liệu thống kê website.</p>
+                                </div>
+
+                            </div><!-- end card header -->
+                        </div>
+                        <!--end col-->
+                    </div>
+                    <!--end row-->
+
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0"> Người dùng
+                                            </p>
+                                        </div>
+                                        {{-- <div class="flex-shrink-0">
+                                       {!! displayStatistical() !!}
+                                    </div> --}}
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h3 class="fs-30 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                    data-target="{{ countAll('user') }}">{{ countAll('user') }}</span> </h3>
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-success-subtle rounded fs-3">
+                                                <i class="bx bx-dollar-circle text-success"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Bài viết</p>
+                                        </div>
+                                        {{-- <div class="flex-shrink-0">
+                                        <h5 class="text-danger fs-14 mb-0">
+                                            <i class="ri-arrow-right-down-line fs-13 align-middle"></i> -3.57 %
+                                        </h5>
+                                    </div> --}}
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h3 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                    data-target="{{ countAll('post') }}">{{ countAll('post') }}</span></h3>
+
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-info-subtle rounded fs-3">
+                                                <i class="bx bx-shopping-bag text-info"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Đăng nhập</p>
+                                        </div>
+                                        <div class="flex-shrink-0">
+                                            {{-- <h5 class="text-success fs-14 mb-0">
+                                            <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +29.08 %
+                                        </h5> --}}
+                                        </div>
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                    data-target="{{ countAll('loginLog') }}">{{ countAll('loginLog') }}</span>
+                                            </h4>
+
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-warning-subtle rounded fs-3">
+                                                <i class="bx bx-user-circle text-warning"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-xl-3 col-md-6">
+                            <!-- card -->
+                            <div class="card card-animate">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex-grow-1 overflow-hidden">
+                                            <p class="text-uppercase fw-medium text-muted text-truncate mb-0"> Khóa học</p>
+                                        </div>
+                                        {{-- <div class="flex-shrink-0">
+                                        <h5 class="text-muted fs-14 mb-0">
+                                            +0.00 %
+                                        </h5>
+                                    </div> --}}
+                                    </div>
+                                    <div class="d-flex align-items-end justify-content-between mt-4">
+                                        <div>
+                                            <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                    data-target="{{ countAll('course') }}">{{ countAll('course') }}</span>
+                                            </h4>
+
+                                        </div>
+                                        <div class="avatar-sm flex-shrink-0">
+                                            <span class="avatar-title bg-primary-subtle rounded fs-3">
+                                                <i class="bx bx-wallet text-primary"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div><!-- end card body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+                    </div> <!-- end row-->
+
+                 
+                </div> <!-- end .h-100-->
+
+            </div> <!-- end col -->
+@livewire('admin.home')
+
+        </div>
+    @endsection
